d3d848c1faeb921873e2244a055070acc34cb04e
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-25 19:41:32 +0530

    ListView: Add functionality to edit task using Update View
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app.views import TaskList, TaskDetail, TaskCreate
+from app.views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task/new/', TaskCreate.as_view(), name='task_new'),
+    path('task/<int:pk>/edit/', TaskUpdate.as_view(), name='task_edit'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from app.models import Task
 
@@ -21,4 +21,10 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     template_name = 'app/task_form.html'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
     success_url = reverse_lazy('tasks')
