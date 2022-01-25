9f9924c72c159e33025aa45d5beb0240cbc83716
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-25 12:46:57 +0530

    app: Create a Create View form for adding new task
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app.views import TaskList, TaskDetail
+from app.views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task/new/', TaskCreate.as_view(), name='task_new'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from app.models import Task
 
@@ -13,3 +15,10 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'app/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'app/task_form.html'
+    success_url = reverse_lazy('tasks')
