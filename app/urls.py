f8899e7a1738a2aeea4a983659aa3bc1aae73eb8
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-25 20:35:18 +0530

    ListView: Add functionality to delete task using Delete View
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from app.views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from app.views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task/new/', TaskCreate.as_view(), name='task_new'),
     path('task/<int:pk>/edit/', TaskUpdate.as_view(), name='task_edit'),
+    path('task/<int:pk>/delete/', TaskDelete.as_view(), name='task_delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from app.models import Task
 
@@ -27,4 +27,10 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
