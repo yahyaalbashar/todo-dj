6bf0be38f159f7687fdf19d6ff967ebcd98ae313
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-26 12:24:08 +0530

    ListView: Add a login page using Login View
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app.views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from app.views import CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task/new/', TaskCreate.as_view(), name='task_new'),
--- app/views.py
+++ app/views.py
@@ -3,7 +3,18 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from django.contrib.auth.views import LoginView
+
 from app.models import Task
+
+
+class CustomLoginView(LoginView):
+    template_name = 'app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 
 class TaskList(ListView):
