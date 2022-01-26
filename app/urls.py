7b1f07023de11f1cead971bce287042b32521ac2
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-26 12:30:01 +0530

    ListView: Add a logout button and send user to login page once he logs out
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from app.views import CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task/new/', TaskCreate.as_view(), name='task_new'),
