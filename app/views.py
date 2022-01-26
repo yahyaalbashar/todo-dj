b6886815379ca419c0ddbd7018826b576661f469
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-26 13:08:29 +0530

    ListView: Show incomplete number of tasks
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/views.py
+++ app/views.py
@@ -25,6 +25,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(name=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
         return context
 
 
