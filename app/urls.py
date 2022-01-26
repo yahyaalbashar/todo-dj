8a835271cda5386232bb942baef5f383d7e87a19
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-26 15:00:24 +0530

    app: Add a Register page for creating user accounts
     * Also redirect user to tasks list if he's already logged in
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from app.views import CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from app.views import CustomLoginView, RegisterView, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from app.models import Task
 
@@ -16,6 +19,23 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+
+class RegisterView(FormView):
+    template_name = 'app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterView, self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
