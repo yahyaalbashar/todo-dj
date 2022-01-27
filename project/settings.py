c5dadc584a3268e945a0340189c0f37a94c1d33a
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-27 20:57:29 +0530

    app: Add styling to whole app
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- project/settings.py
+++ project/settings.py
@@ -119,6 +119,8 @@ LOGIN_URL = 'login'
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
